260b16169d8537605460e8e4f85c298adfc3c028
Xxwen2xxX <142601082+Xxwen2xxX@users.noreply.github.com>
2023-08-18 20:40:54 +0700

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-lis = [money, apple, banana, money, money, money, wiki, space, tv, phone, cucumber, money, money, money]
-count = 1 
-for i in lis:
-    if i == 'money':
-        count +=1
-print(count)
